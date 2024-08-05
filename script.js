dac758fb39b697f16599b5a942958439a7426a28
saiman017 <shresthasaiman017@gmail.com>
2024-08-05 21:25:21 +0545

    javascript modified

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ hamburger.onclick = function () {
   optn = document.querySelector(".options");
   optn.classList.toggle("active");
 };
+
+let logol = document.querySelector(".logo");
+logol.onmouseover = function () {
+  this.style.color = "rgb(157, 92, 221)";
+};
+
+logol.onmouseout = function () {
+  this.style.color = "black";
+};
